7ca2d73151b04f7e50a090798f3b864710879a56
bzanhxy-268 <2684655092@qq.com>
2023-09-20 12:44:16 +0800

    测试版本

---
+++ src/main/java/com/git/demo/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.git.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "测试";
+    }
+}
